a7be8dfc2323b925d859040b454fa83e91df1c30
aseerabbas77 <aseerabbas492@gmail.com>
2026-05-05 00:15:38 -0700

    some new changes added

--- src/config/jwt.ts
+++
@@ -1,6 +0,0 @@
-export const JWT_CONFIG = {
-  ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET || "default_access_secret",
-  REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET || "default_refresh_secret",
-  ACCESS_TOKEN_EXPIRY: "15m",
-  REFRESH_TOKEN_EXPIRY: "30d",
-};
